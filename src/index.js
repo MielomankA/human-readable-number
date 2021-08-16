a163f4f0e8faba6787636c51f15fbad1030b4f22
MielomankA <mielomanka@mail.ru>
2021-08-16 18:20:41 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+  const zero = 'zero';
+  const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  const complexTens = [
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+
+  const getDigit = function (n, dig) {
+    return Math.floor(n / 10 ** dig);
+  };
+
+  const getDigitFromSource = function (source, n, dig) {
+    return source[getDigit(n, dig)];
+  };
+
+  const getRest = function (n, dig) {
+    return n % 10 ** dig;
+  };
+
+  const getOnes = function (n) {
+    return ones[n];
+  };
+
+  const getComplexTens = function (n) {
+    return complexTens[n - 10];
+  };
+
+  const getTens = n => {
+    if (n >= 20) return `${getDigitFromSource(tens, n, 1)} ${getOnes(getRest(n, 1))}`.trim();
+    if (n >= 10 && n < 20) return getComplexTens(n);
+    if (n < 10) return getOnes(n);
+  };
+
+  const getHundreds = n =>
+    n > 99 ? `${getDigitFromSource(ones, n, 2)} hundred ${getTens(getRest(n, 2))}`.trim() : getTens(n);
+
+  return number === 0 ? zero : getHundreds(number);
+};
